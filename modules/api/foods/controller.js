d86d8649b4fb01f9616d31e4f19a1fca0497f571
Thong Do <dovanthong97@gmail.com>
2018-05-20 11:20:18 +0700

    edit getAllFoods

--- modules/api/foods/controller.js
+++ modules/api/foods/controller.js
@@ -1,51 +1,101 @@
 const foodModel = require("./model");
 
-const createFood = ({ name, description, ingredients, steps, timers, imageURL, originalURL }) =>
-    new Promise((resolve, reject) => {
-        imageModel.create({
-            name,
-            description,
-            ingredients,
-            steps,
-            timers,
-            imageURL,
-            originalURL
-        })
-            .then(data => resolve({ id: data._id })) // chi tra lai id
-            .catch(err => reject(err))
+const createFood = ({
+  name,
+  description,
+  ingredients,
+  steps,
+  timers,
+  imageURL,
+  originalURL
+}) =>
+  new Promise((resolve, reject) => {
+    imageModel
+      .create({
+        name,
+        description,
+        ingredients,
+        steps,
+        timers,
+        imageURL,
+        originalURL
+      })
+      .then(data => resolve({ id: data._id })) // chi tra lai id
+      .catch(err => reject(err));
+  });
+
+const getAllFoodsByCondition = userInput =>
+  new Promise((resolve, reject) => {
+    foodModel
+      .find()
+      .exec()
+      .select("_id name description ingredients")
+      .then(async function(foods) {
+        let result = await filterFood(foods);
+        resolve(result);
+      })
+      .catch(err => reject(err));
+  });
+
+function filterFood(foods) {
+  var result = [];
+  if (!userInput) {
+    foods.forEach(food => {
+      if (userInput.length > food.ingredients.length) {
+        var flag = false;
+        food.ingredients.every(item => {
+          if (!userInput.includes(item)) {
+            flag = true;
+          }
+        });
+        if (!flag) {
+          result.push(food);
+        }
+      }
+
+      if (
+        userInput.length < food.ingredients.length &&
+        userInput.ingredients.length > food.length - 3
+      ) {
+        var flag = false;
+        userInput.every(item => {
+          if (!food.ingredients.includes(item)) {
+            flag = true;
+          }
+        });
+        if (!flag) {
+          result.push(food);
+        }
+      }
     });
-
-const getAllFoods = () => new Promise((resolve, reject) => {
-    foodModel.find()
-        .exec()
-        .select("_id name description ingredients")
-        .then(data => resolve(data))
-        .catch(err => reject(err))
-});
-
-const getFood = id => new Promise((resolve, reject) => {
-    foodModel
-        .findOne({
-            _id: id
-        })
-        .exec()
-        .then(data => resolve(data))
-        .catch(err => reject(err))
-});
-
-const getAllIngredients = () => new Promise((resolve, reject) => {
-    foodModel
-        .find()
-        .select("_id ingredients ")
-        .exec()
-        .then(data => resolve(data))
-        .catch(err => reject(err))
-});
-
-module.exports = {
-    createFood,
-    getAllFoods,
-    getFood,
-    getAllIngredients
+  }
+  return result;
 }
 
+const getFood = id =>
+  new Promise((resolve, reject) => {
+    foodModel
+      .findOne({
+        _id: id
+      })
+      .exec()
+      .then(data => resolve(data))
+      .catch(err => reject(err));
+  });
+
+const getAllIngredients = () =>
+  new Promise((resolve, reject) => {
+    foodModel
+      .find()
+      .select("_id ingredients ")
+      .exec()
+      .then(data => resolve(data))
+      .catch(err => reject(err));
+  });
+
+module.exports = {
+  createFood,
+  getAllFoods,
+  getFood,
+  getAllIngredients
+};
